f02f90153e18af92c1adbf30b7b512d705ae2b8a
lalitha12-web <65554162+lalitha12-web@users.noreply.github.com>
2023-12-14 17:11:27 +0530

    Update ApplicantAPIService.js

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -1,8 +1,8 @@
 
 import axios from 'axios';
-export const apiUrl = 'http://localhost:8081';
+//export const apiUrl = 'http://localhost:8081';
 
-//export const apiUrl = 'https://talentstreambackendversion2.onrender.com';
+export const apiUrl = 'https://talentstreambackendversion2.onrender.com';
 
 
 const ApplicantAPIService = {
